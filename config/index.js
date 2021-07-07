a590468a6f4fca48c4445124ad65d6c4c6e192d5
alferno <aniandp@gmail.com>
2021-07-08 01:00:15 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'Production Website'
+export const server = dev ? 'http://localhost:3000' : 'https://angry-stonebraker-993759.netlify.app/'
